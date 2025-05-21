f53f1cf0aca454ee3d1506a12f9be7d88b008919
iuryyCosta <iury.costa2209@gmail.com>
2025-05-21 14:50:43 -0300

    Atualização Main ajuste atividade

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Criando um carro do tipo Sedan
-        Carro meuSedan = new Sedan("Civic", 4);
-        
-        Carro meuSuv = new Suv("HR-V", 5);
-        // Exibindo informações do SUV
-        
-        meuSuv.exibirInfo();
 
-        // Exibindo informações do carro
-        meuSedan.exibirInfo();
+        List<Carro> listaDeCarros = new ArrayList<>();
+
+        listaDeCarros.add(new Sedan("Civic", 4));
+        listaDeCarros.add(new Suv("HR-V", 5));
+        listaDeCarros.add(new Sedan("Corolla", 4));
+        listaDeCarros.add(new Suv("Tracker", 5));
+   
+        
+         for (Carro carro : listaDeCarros) {
+            carro.exibirInfo();
+            System.out.println("--------------------");
+        }
     }
 }
